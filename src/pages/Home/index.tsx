00e00790c73c523ac2a850a21bcb93b40feda98b
siddharth <me@siddharthjain.in>
2024-04-13 17:32:12 +0530

    not stopping

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,15 @@
 import { Component } from "solid-js";
-
+import { Button } from "~/components/ui/button";
 const Home: Component = () => {
   return (
-    <div class="flex flex-col">
-      <div class="text-2xl font-bold text-primary leading-10">Dashboard</div>
+    <div class="flex flex-col flex-1 flex-grow overflow-hidden">
+      <div class="flex justify-between items-center p-6 ">
+        <div class="text-2xl font-bold text-primary leading-10">Dashboard</div>
+
+        <Button class="font-bold text-base" variant={"secondary"} size="lg">
+          Create Dashboard
+        </Button>
+      </div>
     </div>
   );
 };
