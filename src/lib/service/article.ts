9af76d6c2956fb85dc6964c72ea1b3063872a68b
siddharth <me@siddharthjain.in>
2024-04-26 19:48:08 +0530

    only origin

--- src/lib/service/article.ts
+++ src/lib/service/article.ts
@@ -2,20 +2,20 @@ import { get_token } from "@lib/utils";
 import { Effect } from "effect";
 import { err, ok } from "neverthrow";
 
+// Assuming the environment variable is named REACT_APP_API_URL
+const API_URL = import.meta.env.VITE_STRAPI_URL as string;
+
 const fetch_articles = async () => {
   try {
     const token = get_token();
-
     const reqHeaders = new Headers();
-
     reqHeaders.append("Authorization", `Bearer ${token}`);
-
     console.log({ reqHeaders });
 
     const getArticleEffect = () =>
       Effect.tryPromise({
         try: () =>
-          fetch("http://localhost:1337/api/articles", {
+          fetch(`${API_URL}/api/articles`, {
             headers: reqHeaders,
             method: "GET",
           }),
@@ -36,20 +36,17 @@ const fetch_articles = async () => {
 const save_article = async (data: any) => {
   try {
     const token = get_token();
-
     const reqHeaders = new Headers();
-
     reqHeaders.append("Authorization", `Bearer ${token}`);
     reqHeaders.append("Content-Type", "application/json");
 
-    const response = await fetch("http://localhost:1337/api/articles", {
+    const response = await fetch(`${API_URL}/api/articles`, {
       method: "POST",
       headers: reqHeaders,
       body: JSON.stringify({ data }),
     });
 
     const result = await response.json();
-
     console.log("saving article", result);
 
     if (!response.ok) {
@@ -70,12 +67,10 @@ const fetch_article_by_id = async (id: string) => {
 
   try {
     const token = get_token();
-
     const reqHeaders = new Headers();
-
     reqHeaders.append("Authorization", `Bearer ${token}`);
 
-    const response = await fetch(`http://localhost:1337/api/articles/${id}`, {
+    const response = await fetch(`${API_URL}/api/articles/${id}`, {
       headers: reqHeaders,
       method: "GET",
     });
@@ -101,10 +96,9 @@ const delete_article = async (id: string) => {
 
   try {
     const reqHeaders = new Headers();
-
     reqHeaders.append("Authorization", `Bearer ${token}`);
 
-    const response = await fetch(`http://localhost:1337/api/articles/${id}`, {
+    const response = await fetch(`${API_URL}/api/articles/${id}`, {
       headers: reqHeaders,
       method: "DELETE",
     });
