6f5da4dd2b86f265c7381e4f719628e09987b920
siddharth <me@siddharthjain.in>
2024-05-01 17:48:06 +0530

    mobile UI done

--- src/components/bare/Nav.tsx
+++ src/components/bare/Nav.tsx
@@ -6,7 +6,7 @@ export const Nav: Component = (props) => {
   return (
     <header class="bg-secondary py-4 px-6 flex-shrink-0">
       <nav class="mx-auto flex items-center justify-between">
-        <div class="text-3xl font-black flex items-baseline gap-2 justify-between">
+        <div class="text-3xl font-black flex items-baseline gap-2 justify-between sm:flex-row flex-col">
           <A href="/">
             <div class="">
               <span class="text-text-inverted">ORANGE</span>
