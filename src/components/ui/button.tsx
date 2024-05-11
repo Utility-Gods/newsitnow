d10c6c9b5586ae30057ff5635d3f47bebb2badde
siddharth <me@siddharthjain.in>
2024-05-11 20:44:20 +0530

    article publish unpubluish done

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,17 +1,17 @@
-import type { Component, ComponentProps } from "solid-js"
-import { splitProps } from "solid-js"
+import type { Component, ComponentProps } from "solid-js";
+import { splitProps } from "solid-js";
 
-import type { VariantProps } from "class-variance-authority"
-import { cva } from "class-variance-authority"
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
 
-import { cn } from "~/lib/utils"
+import { cn } from "~/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex font-semibold items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-text hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -32,7 +32,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
+  },
 );
 
 export interface ButtonProps
@@ -40,13 +40,16 @@ export interface ButtonProps
     VariantProps<typeof buttonVariants> {}
 
 const Button: Component<ButtonProps> = (props) => {
-  const [, rest] = splitProps(props, ["variant", "size", "class"])
+  const [, rest] = splitProps(props, ["variant", "size", "class"]);
   return (
     <button
-      class={cn(buttonVariants({ variant: props.variant, size: props.size }), props.class)}
+      class={cn(
+        buttonVariants({ variant: props.variant, size: props.size }),
+        props.class,
+      )}
       {...rest}
     />
-  )
-}
+  );
+};
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
